c326e64660e07fcfa005640929af21792b94685f
Raul Figueroa <rfigueroa17@hotmail.com>
2022-01-08 17:46:25 -0700

    add list sorting by parameters

--- src/components/thumbnailContainer/index.js
+++ src/components/thumbnailContainer/index.js
@@ -1,15 +1,20 @@
 /* eslint-disable no-unused-vars, max-len */
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { nextCountries, nextElements } from '../../redux/dataReducer';
+import {
+  nextCountries, nextElements, setMainFilter, sortList,
+} from '../../redux/dataReducer';
 import MainThumbnail from '../mainThumbnail';
 import Thumbnail from '../thumbnail';
 import './style.css';
 
-function ThumbnailContainer(props) {
+function ThumbnailContainer() {
   const dispatch = useDispatch();
+
   const data = useSelector((state) => state.dataReducer);
+
   const nextToShow = [];
+
   const previous = () => {
     const nextIndex = data[0].map((e) => e - 10);
     dispatch(nextElements(nextIndex));
@@ -18,6 +23,7 @@ function ThumbnailContainer(props) {
     }
     dispatch(nextCountries(nextToShow));
   };
+
   const next = () => {
     const nextIndex = data[0].map((e) => e + 10);
     dispatch(nextElements(nextIndex));
@@ -26,18 +32,122 @@ function ThumbnailContainer(props) {
     }
     dispatch(nextCountries(nextToShow));
   };
+
+  const filterElements = (paramToFilter) => {
+    let filteredArr = [];
+    switch (paramToFilter) {
+      case '1': {
+        filteredArr = data[1].sort((a, b) => (a.cases < b.cases ? 1 : -1));
+        dispatch(setMainFilter(['cases', filteredArr[0]]));
+        break;
+      }
+      case '2': {
+        filteredArr = data[1].sort((a, b) => (a.todayCases < b.todayCases ? 1 : -1));
+        dispatch(setMainFilter(['todayCases', filteredArr[0]]));
+        break;
+      }
+      case '3': {
+        filteredArr = data[1].sort((a, b) => (a.recovered < b.recovered ? 1 : -1));
+        dispatch(setMainFilter(['recovered', filteredArr[0]]));
+        break;
+      }
+      case '4': {
+        filteredArr = data[1].sort((a, b) => (a.critical < b.critical ? 1 : -1));
+        dispatch(setMainFilter(['critical', filteredArr[0]]));
+        break;
+      }
+      case '5': {
+        filteredArr = data[1].sort((a, b) => (a.deaths < b.deaths ? 1 : -1));
+        dispatch(setMainFilter(['deaths', filteredArr[0]]));
+        break;
+      }
+      default:
+        return null;
+    }
+    return filteredArr;
+  };
+
+  const mainThumbnailChange = (e) => {
+    filterElements(e.target.value);
+  };
+
+  const sortListElements = (paramToFilter) => {
+    let filteredArr = [];
+    switch (paramToFilter) {
+      case '1': {
+        filteredArr = data[1].sort((a, b) => (a.cases < b.cases ? 1 : -1));
+        dispatch(sortList(filteredArr));
+        break;
+      }
+      case '2': {
+        filteredArr = data[1].sort((a, b) => (a.todayCases < b.todayCases ? 1 : -1));
+        dispatch(sortList(filteredArr));
+        break;
+      }
+      case '3': {
+        filteredArr = data[1].sort((a, b) => (a.recovered < b.recovered ? 1 : -1));
+        dispatch(sortList(filteredArr));
+        break;
+      }
+      case '4': {
+        filteredArr = data[1].sort((a, b) => (a.critical < b.critical ? 1 : -1));
+        dispatch(sortList(filteredArr));
+        break;
+      }
+      case '5': {
+        filteredArr = data[1].sort((a, b) => (a.deaths < b.deaths ? 1 : -1));
+        dispatch(sortList(filteredArr));
+        break;
+      }
+      default:
+        return null;
+    }
+    return filteredArr;
+  };
+
+  const ThumbnailContainerChange = (e) => {
+    sortListElements(e.target.value);
+    for (let i = 0; i < 10; i += 1) {
+      nextToShow.push(data[1][i]);
+    }
+    dispatch(nextCountries(nextToShow));
+  };
+
   return (
     <div className="row justify-content-center" data-test="thumbnailContainer">
+      <div className="col-12 row justify-content-between">
+        <p className="col-3">Most: </p>
+        <select className="col-9" onChange={(e) => mainThumbnailChange(e)}>
+          <option value={1}>Total cases</option>
+          <option value={2}>Today cases</option>
+          <option value={3}>Total recovered</option>
+          <option value={4}>Critical state patients</option>
+          <option value={5}>Deaths</option>
+        </select>
+      </div>
       <div className="col-12">
+        {data[3]
+        && (
         <MainThumbnail
-          country="demo country"
-          flag="https://disease.sh/assets/img/flags/us.png"
-          cases={100}
-          todayCases={14}
+          country={data[3][1].country}
+          flag={data[3][1].flag}
+          cases={data[3][1].cases}
+          todayCases={data[3][1].todayCases}
         />
+        )}
       </div>
       <div className="col-12 statsHeader">
         <p>Stats by country</p>
+      </div>
+      <div className="col-12 row justify-content-between">
+        <p className="col-4">Sort by: </p>
+        <select className="col-8" onChange={(e) => ThumbnailContainerChange(e)}>
+          <option value={1}>Total cases</option>
+          <option value={2}>Today cases</option>
+          <option value={3}>Total recovered</option>
+          <option value={4}>Critical state patients</option>
+          <option value={5}>Deaths</option>
+        </select>
       </div>
       {data[2]
         && data[2].map((e) => (
--- src/redux/dataReducer.js
+++ src/redux/dataReducer.js
@@ -1,10 +1,12 @@
-/* eslint-disable no-unused-vars */
+/* eslint-disable no-unused-vars, prefer-destructuring */
 import axios from 'axios';
 
 export const GET_DATA = 'covid-tracking/dataReducer/GET_DATA';
 export const SET_COUNTRIES_TO_SHOW = 'covid-tracking/dataReducer/SET_COUNTRIES_TO_SHOW';
 export const NEXT_ELEMENTS = 'covid-tracking/dataReducer/NEXT_ELEMENTS';
 export const PREV_ELEMENTS = 'covid-tracking/dataReducer/PREV_ELEMENTS';
+export const MAIN_FILTER = 'covid-tracking/dataReducer/MAIN_FILTER';
+export const SORT_LIST = 'covid-tracking/dataReducer/SORT_LIST';
 
 export const fetchData = () => async (dispatch, getState) => {
   await axios.get('https://disease.sh/v3/covid-19/countries')
@@ -27,9 +29,10 @@ export const fetchData = () => async (dispatch, getState) => {
       });
       const indexToShow = getState().dataReducer[0];
       const toShow = arr.filter((e, index) => index === indexToShow[index]);
+      const setMainFilter = ['cases', arr[3]];
       dispatch({
         type: GET_DATA,
-        payload: [arr, toShow],
+        payload: [arr, toShow, setMainFilter],
       });
     });
 };
@@ -44,12 +47,22 @@ export const nextCountries = (arr) => ({
   payload: arr,
 });
 
+export const setMainFilter = (arr) => ({
+  type: MAIN_FILTER,
+  payload: arr,
+});
+
+export const sortList = (arr) => ({
+  type: SORT_LIST,
+  payload: arr,
+});
+
 const initialState = [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]];
 
 export const dataReducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_DATA:
-      return [...state, action.payload[0], action.payload[1]];
+      return [...state, ...action.payload];
     case SET_COUNTRIES_TO_SHOW: {
       const newState = [...state];
       newState[2] = action.payload;
@@ -60,7 +73,17 @@ export const dataReducer = (state = initialState, action) => {
       newState[0] = action.payload;
       return newState;
     }
-
+    case MAIN_FILTER: {
+      const newState = [...state];
+      newState[3][0] = action.payload[0];
+      newState[3][1] = action.payload[1];
+      return newState;
+    }
+    case SORT_LIST: {
+      const newState = [...state];
+      newState[1] = action.payload;
+      return newState;
+    }
     default:
       return state;
   }
